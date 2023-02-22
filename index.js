8e49485b1877607d8d8e1f21bbe33689cdc61824
Adrian Goding <godingadrian@gmail.com>
2023-02-22 15:03:02 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// function sayHello(){
-//     console.log("hello world");
-// }
-
-
-// function sayHi(){
-//     sayHello();
-// }
-// sayHi();
-
-// function sayBye(goodBye){
-//     goodBye();
-// }
-// sayBye(sayHi);
-
 function receivesAFunction(h1){
     console.log(h1());
 }
